2846ef74f37c697a982ec1b1ca997f98fc0146d0
1234567890server <d1032599@gmail.com>
2025-12-27 18:29:46 +0300

    Update chat.js

--- chat.js
+++ chat.js
@@ -1,24 +1,52 @@
+const SUPABASE_URL = "https://bncysgnqsgpdpuupzgqj.supabase.co";
+const SUPABASE_KEY = "sb_publishable_bCoFKBILLDgxddAOkd0ZrA_7LJTvSaR";
+
+const supabase = window.supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_KEY
+);
+
 const messagesDiv = document.getElementById("messages");
+const usernameInput = document.getElementById("username");
+const textInput = document.getElementById("text");
+const sendBtn = document.getElementById("send");
 
-let messages = [];
+// загрузка сообщений
+async function loadMessages() {
+  const { data, error } = await supabase
+    .from("messages")
+    .select("*")
+    .order("created_at", { ascending: true });
 
-function send() {
-  const name = document.getElementById("name").value.trim();
-  const text = document.getElementById("text").value.trim();
+  if (error) return;
 
-  if (!name || !text) return;
-
-  messages.push({ name, text });
-  document.getElementById("text").value = "";
-
-  render();
-}
-
-function render() {
   messagesDiv.innerHTML = "";
-  messages.forEach(m => {
-    const div = document.createElement("div");
-    div.textContent = m.name + ": " + m.text;
-    messagesDiv.appendChild(div);
+
+  data.forEach(msg => {
+    const el = document.createElement("div");
+    el.textContent = ${msg.username}: ${msg.text};
+    messagesDiv.appendChild(el);
   });
+
+  messagesDiv.scrollTop = messagesDiv.scrollHeight;
 }
+
+// отправка сообщения
+sendBtn.onclick = async () => {
+  const username = usernameInput.value.trim();
+  const text = textInput.value.trim();
+  if (!username || !text) return;
+
+  await supabase.from("messages").insert({
+    username,
+    text
+  });
+
+  textInput.value = "";
+};
+
+// первая загрузка
+loadMessages();
+
+// каждые 2 секунды обновляем
+setInterval(loadMessages, 2000);
