17f10be6c5c231c73c4be4cef79274822903ed89
1234567890server <d1032599@gmail.com>
2026-03-01 18:31:50 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,24 +1,10 @@
 const CACHE_NAME = 'speednexus-v1';
 const urlsToCache = [
-  '/',
-  '/index.html',
-  '/style.css',
-  '/chat.js',
-  '/manifest.json',
-  '/icon-192.png',
-  '/icon-512.png'
+  '/static/',
+  '/static/index.html',
+  '/static/style.css',
+  '/static/chat.js',
+  '/static/manifest.json',
+  '/static/icon-192.png',
+  '/static/icon-512.png'
 ];
-
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
-  );
-});
-
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
-  );
-});
