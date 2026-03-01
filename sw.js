6066859d8c163f743fb58c39ed50d63b20832716
1234567890server <d1032599@gmail.com>
2026-03-01 18:33:55 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -8,3 +8,17 @@ const urlsToCache = [
   '/static/icon-192.png',
   '/static/icon-512.png'
 ];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
